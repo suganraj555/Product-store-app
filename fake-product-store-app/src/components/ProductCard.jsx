ceb0f5e29f541448a4151c5e02eff2ef22cb2891
suganraj555 <105486612+suganraj555@users.noreply.github.com>
2025-04-12 08:11:58 +0530

    ProductCard.jsx

--- fake-product-store-app/src/components/ProductCard.jsx
+++ fake-product-store-app/src/components/ProductCard.jsx
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom';
 const ProductCard = ({ product }) => {
   return (
     <div className="col">
-      <div className="card h-100 shadow-sm border-0 transition">
+      <div className="card h-100">
         <img
           src={product.image}
-          className="card-img-top p-4"
+          className="p-4"
           alt={product.title}
           style={{ height: '250px', objectFit: 'contain' }}
         />
-        <div className="card-body d-flex flex-column justify-content-between">
-          <h5 className="card-title">{product.title}</h5>
-          <p className="card-text fw-semibold text-success">Only ${product.price}</p>
-          <p className="card-text small">
+        <div className="card-body d-flex flex-column">
+          <h5>{product.title}</h5>
+          <p className="fw-semibold text-success">Only ${product.price}</p>
+          <p>
             ⭐ Rated {product.rating?.rate} / 5 by {product.rating?.count} users
           </p>
           <Link to={`/product/${product.id}`} className="btn btn-outline-primary mt-auto">
